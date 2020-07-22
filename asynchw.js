e3ff609fcd2e6f145b1e02328f83d7fe0282bde2
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2020-07-22 10:22:08 +0800

    add solution for first day homework

--- asynchw.js
+++ asynchw.js
@@ -3,3 +3,24 @@
 // fs.readFile
 // fs.appendFile
 // Use example from the documentation to help : https://nodejs.org/api/fs.html
+
+var fs = require("fs");
+
+console.log("Begin reading File");
+fs.readFile("input.txt",(err,data)=>{  // function(err,data){}
+	if (err) console.log(err)
+	console.log(data.toString());
+	console.log("End reading file");
+	console.log("Appending something");
+	fs.appendFile("input.txt","I am appending something", (err)=>{
+		if (err) console.log(err);
+		console.log("Succesfully appending something")
+		console.log("Begin re-reading file")
+		/////////////////////
+		fs.readFile("input.txt", (err,data)=>{
+			if (err) console.log(err);
+			console.log(data.toString())
+			console.log("Finish re-reading file")
+		})
+	})
+})
